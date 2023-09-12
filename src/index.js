97aac93d636db3404c5c75958a7da23e48b6a7c7
Gabor Varga <varga4x4@outlook.com>
2023-09-12 20:41:40 +0100

    Add endButtonElement.onclick function

--- src/index.js
+++ src/index.js
@@ -73,8 +73,11 @@ const IN_CALL_BUTTONS_ARRAY = [
         const displayElement = document.getElementById("display")
         const callButtonElement = document.getElementById("call-button")
         const deleteButtonElement = document.getElementById("delete-button")
-        const dialButtonsElement = document.getElementById("dial-buttons")
+        const dialButtonPadElement = document.getElementById("dial-button-pad")
         const callDeleteWrapperElement = document.getElementById("call-delete-wrapper")
+        const footerElement = document.querySelector("footer")
+        const appElement = document.getElementById("app")
+        const mainElement = document.querySelector("main")
 
         // HELPERS
         const renderDisplay = (numberToAdd) => {
@@ -93,6 +96,9 @@ const IN_CALL_BUTTONS_ARRAY = [
 
             if (newValue.length) {
                 deleteButtonElement.style.visibility = "visible"
+                callButtonElement.onclick = () => {
+                    renderDialButtons()
+                }
             } else {
                 deleteButtonElement.style.visibility = "hidden"
                 addNumberElement.style.visibility = "hidden"
@@ -103,61 +109,86 @@ const IN_CALL_BUTTONS_ARRAY = [
             addNumberElement.innerText = "calling..."
 
             document.querySelectorAll('.dial-button').forEach((oldButton) => oldButton.remove())
+            document.querySelectorAll('footer').forEach((oldFooter) => oldFooter.remove())
+            document.querySelectorAll('#call-delete-wrapper').forEach((oldwrapper) => oldwrapper.remove())
 
             IN_CALL_BUTTONS_ARRAY.forEach((button) => {
                 const inCallButtonElement = document.createElement("button")
                 inCallButtonElement.className = "dial-button"
                 inCallButtonElement.innerHTML = button.value
 
-                dialButtonsElement.appendChild(inCallButtonElement)
+                dialButtonPadElement.appendChild(inCallButtonElement)
             })
 
+            const endButtonElementWrapper = document.createElement("div")
+            endButtonElementWrapper.id = "end-button-element-wrapper"
+            endButtonElementWrapper.style.border = "2px solid red"
+            endButtonElementWrapper.style.paddingTop = "100px"
+            endButtonElementWrapper.style.paddingBottom = "100px"
+            endButtonElementWrapper.style.display = "flex"
+            endButtonElementWrapper.style.justifyContent = "center"
+
             const endButtonElement = document.createElement("button")
+            endButtonElement.id = "end-button"
             endButtonElement.className = "dial-button"
             endButtonElement.innerHTML = "End"
+            endButtonElement.style.background = "red"
+            endButtonElement.style.color = "white"
 
-            callDeleteWrapperElement.appendChild(endButtonElement)
+            mainElement.appendChild(endButtonElementWrapper)
+            endButtonElementWrapper.appendChild(endButtonElement)
+
+            // END_BUTTON_ELEMENT
+            endButtonElement.onclick = () => {
+                document.querySelectorAll('.dial-button').forEach((oldButton) => oldButton.remove())
+                document.querySelectorAll("#end-button-element-wrapper").forEach((oldwrapper) => oldwrapper.remove())
+
+                mainElement.appendChild(callDeleteWrapperElement)
+                callDeleteWrapperElement.appendChild(callButtonElement)
+                callDeleteWrapperElement.appendChild(deleteButtonElement)
+                appElement.appendChild(footerElement)
+
+                renderDisplay()
+                renderApp()
+            }
         }
 
-        // ADD_NUMBER_ELEMENT
-        addNumberElement.innerText = "Add Number"
-        addNumberElement.style.visibility = "hidden"
-        addNumberElement.onclick = () => {
-            console.log("a")
-        }
-
-        // DELETE_BUTTON_ELEMENT
-        deleteButtonElement.style.visibility = "hidden"
-        deleteButtonElement.onclick = () => {
-            renderDisplay(undefined)
-        }
-
-        // CALL_BUTTON_ELEMENT
-        callButtonElement.onclick = () => {
-            renderDialButtons()
-        }
-
-        DIAL_BUTTONS_ARRAY.forEach((button) => {
-            const dialButtonElement = document.createElement("button")
-            dialButtonElement.className = "dial-button"
-
-            dialButtonElement.onclick = () => {
-                const numberToAdd = buttonValueElement.innerHTML
-                renderDisplay(numberToAdd)
+        const renderApp = () => {
+            // ADD_NUMBER_ELEMENT
+            addNumberElement.innerText = "Add Number"
+            addNumberElement.style.visibility = "hidden"
+            addNumberElement.onclick = () => {
+                console.log("a")
             }
 
-            const buttonValueElement = document.createElement('p')
-            buttonValueElement.innerHTML = button.value
-            buttonValueElement.className = "dial-values"
-            dialButtonElement.appendChild(buttonValueElement)
-
-            if (button.characters !== undefined) {
-                const buttonCharactersElement = document.createElement('p')
-                buttonCharactersElement.innerHTML = button.characters
-                dialButtonElement.appendChild(buttonCharactersElement)
+            // DELETE_BUTTON_ELEMENT
+            deleteButtonElement.style.visibility = "hidden"
+            deleteButtonElement.onclick = () => {
+                renderDisplay(undefined)
             }
 
-            dialButtonsElement.appendChild(dialButtonElement)
+            DIAL_BUTTONS_ARRAY.forEach((button) => {
+                const dialButtonElement = document.createElement("button")
+                dialButtonElement.className = "dial-button"
 
-        })
+                dialButtonElement.onclick = () => {
+                    const numberToAdd = buttonValueElement.innerHTML
+                    renderDisplay(numberToAdd)
+                }
+
+                const buttonValueElement = document.createElement('p')
+                buttonValueElement.innerHTML = button.value
+                buttonValueElement.className = "dial-values"
+                dialButtonElement.appendChild(buttonValueElement)
+
+                if (button.characters !== undefined) {
+                    const buttonCharactersElement = document.createElement('p')
+                    buttonCharactersElement.innerHTML = button.characters
+                    dialButtonElement.appendChild(buttonCharactersElement)
+                }
+
+                dialButtonPadElement.appendChild(dialButtonElement)
+            })
+        }
+        renderApp()
     })()
